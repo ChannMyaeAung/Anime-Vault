f7277b2a7e112e3a8a612fb9ab2507a00549b195
ChannMyaeAung <chanmyaeaung@my.jcu.edu.au>
2024-01-17 00:30:31 +0630

    implemented animecard component and added to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import { fetchAnime } from "./action";
+import AnimeCard, { AnimeProps } from "@/components/AnimeCard";
 
 export default async function Home() {
   const data = await fetchAnime(1);
   console.log(data);
   return (
-    <main>
-      <h1>Explore Anime</h1>
+    <main className="flex flex-col gap-12 px-8 py-16 sm:px-10">
+      <h1 className="text-3xl font-semibold text-white ">Explore Anime</h1>
+      <section className="grid grid-cols-1 gap-12 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {data.map((item: AnimeProps, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
     </main>
   );
 }
--- components/AnimeCard.tsx
+++ components/AnimeCard.tsx
@@ -1,7 +1,72 @@
+import Image from "next/image";
 import React from "react";
 
-const AnimeCard = () => {
-  return <div>AnimeCard</div>;
+export interface AnimeProps {
+  id: string;
+  name: string;
+  image: {
+    original: string;
+  };
+  kind: string;
+  episodes: number;
+  episodes_aired: number;
+  score: string;
+}
+
+interface Props {
+  anime: AnimeProps;
+  index: number;
+}
+
+const AnimeCard = ({ anime, index }: Props) => {
+  return (
+    <div className="relative w-full max-w-sm rounded">
+      <div className="relative w-full h-[37vh]">
+        <Image
+          src={`https://shikimori.one${anime.image.original}`}
+          alt={anime.name}
+          fill
+          className="rounded-xl"
+        />
+      </div>
+
+      <article className="flex flex-col gap-3 py-4">
+        <div className="flex items-center justify-between gap-1">
+          <h3 className="text-xl font-medium text-neutral-100 line-clamp-1">
+            {anime.name}
+          </h3>
+          <h4 className="text-sm font-bold uppercase">{anime.kind}</h4>
+        </div>
+        <div className="flex items-center gap-4">
+          <div className="flex items-center gap-1">
+            <Image
+              src={"/episodes.svg"}
+              alt="episodes"
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <span className="text-base font-bold text-white">
+              {anime.episodes || anime.episodes_aired}
+            </span>
+          </div>
+
+          <div className="flex items-center gap-1">
+            <Image
+              src={"/star.svg"}
+              alt="episodes"
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <span className="text-[#FFAD49] text-base font-bold">
+              {anime.score}
+            </span>
+          </div>
+        </div>
+      </article>
+    </div>
+  );
 };
 
 export default AnimeCard;

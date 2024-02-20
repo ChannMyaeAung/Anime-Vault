1ef95e9a1bb0e625a5f94cf8b4a271c317ca87ca
ChannMyaeAung <chanmyaeaung@my.jcu.edu.au>
2024-02-20 13:50:37 +0800

    removed form and general styling changes in the hero component.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,25 +1,23 @@
 import Image from "next/image";
 import React from "react";
-import Form from "./Form";
 
 const Hero = () => {
   return (
-    <div className="container flex flex-col h-full min-h-screen gap-12 px-2 py-8 mx-auto md:gap-16 md:px-8 md:py-16 bg-slate-800">
-      <div className="flex flex-col gap-6 md:flex-row">
+    <div className="container flex flex-col justify-between h-full min-h-screen gap-12 px-2 py-8 mx-auto md:gap-16 md:px-8 md:py-16 bg-[radial-gradient(145.05%_100%_at_50%_0,#1d2b41_0,#020509_57.38%,#0f1a29_88.16%)] ">
+      <div className="flex flex-col items-center justify-center h-full gap-6 p-3 md:flex-row">
         <Image
           src="./logo.svg"
           alt="logo"
           width={101}
           height={96}
-          className="flex-shrink-0 object-contain"
+          className="flex-shrink-0 object-contain "
         />
-        <h1 className="text-5xl font-semibold text-neutral-200 page-title">
-          The best site to watch anime online for Free!
+        <h1 className="text-4xl font-bold text-center sm:text-5xl lg:text-6xl md:text-start  text-transparent bg-clip-text overflow-hidden bg-[radial-gradient(138.06%_1036.51%_at_95.25%_-2.54%,#7ed4fd_14.06%,#709df7_51.02%,#4d78ef_79.09%)] p-3">
+          Browse Popular Anime
         </h1>
       </div>
 
       <div className="relative flex flex-col gap-12">
-        <Form />
         <div className="self-center">
           <Image
             src={"/hero.webp"}
